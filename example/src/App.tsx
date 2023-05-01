39848efaf118d1da8bef0a779f58ddbcf1d970ee
Jonathon Orsi <jonathon.orsi@gmail.com>
2023-05-01 00:59:53 -0400

    experiment on more atomic game hooks/components; add rudimentary audio api; add input hooks for key, mouse, and gamepad

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,68 +1,481 @@
-import { NavLink, Route, Routes } from "react-router-dom";
-import Pong from "./pong";
-
-import RPG from "./rpg";
-
-const routes = [
-  {
-    name: "Pong",
-    path: "/",
-    element: <Pong />,
-  },
-  {
-    name: "RPG",
-    path: "/rpg",
-    element: <RPG />,
-  },
-];
+import {
+  createContext,
+  useRef,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+  useContext,
+  useState,
+  useCallback,
+} from "react";
 
 export default function App() {
+  const ref = useRef<Entity>();
+  const [transform, setTransform] = useState({ x: 500, y: 200, z: 0 });
+
+  // audio
+  const sample = useAudio("/beep-03.wav");
+
+  useGamepad(
+    "Up",
+    () => {
+      setTransform({
+        ...transform,
+        y: transform.y - 5,
+      });
+    },
+    true
+  );
+
+  useGamepad(
+    "Down",
+    () => {
+      setTransform({
+        ...transform,
+        y: transform.y + 5,
+      });
+    },
+    true
+  );
+
+  useGamepad(
+    "Left",
+    () => {
+      setTransform({
+        ...transform,
+        x: transform.x - 5,
+      });
+    },
+    true
+  );
+
+  useGamepad(
+    "Right",
+    () => {
+      setTransform({
+        ...transform,
+        x: transform.x + 5,
+      });
+    },
+    true
+  );
+
+  // render
   return (
     <div
       style={{
-        display: "flex",
-        height: "100%",
+        position: "absolute",
+        top: `${transform.y}px`,
+        left: `${transform.x}px`,
+        background: "white",
+        width: "50px",
+        height: "50px",
       }}
-    >
-      <div
-        style={{
-          backgroundColor: "#191919",
-          color: "black",
-          height: "100%",
-          width: "180px",
-          padding: "24px",
-        }}
-      >
-        <ul style={{ listStyle: "none", margin: "0", padding: "0" }}>
-          {routes.map((route) => (
-            <li key={`${route.path}`}>
-              <NavLink to={route.path}>{route.name}</NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          flex: "1 0 auto",
-          height: "100%",
-          padding: "24px",
-          placeItems: "center",
-          placeContent: "center",
-        }}
-      >
-        <div>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.name}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Routes>
-        </div>
-      </div>
-    </div>
+    ></div>
   );
 }
+
+///
+
+type Component<K extends string, T> = {
+  name: K;
+  defaultValue: T;
+};
+
+const createComponent = <K extends string, T>(
+  name: K,
+  defaultValue: T
+): Component<K, T> => {
+  return {
+    name,
+    defaultValue,
+  };
+};
+
+const useComponent = <K extends string, T>(component: Component<K, T>): T => {
+  const entity = useContext<Entity<any>>(EntityContext);
+  if (entity == null) {
+    return;
+  }
+
+  return entity[component["name"]];
+};
+
+const IdComponent = createComponent("id", "");
+const RenderComponent = createComponent("render", (entity: Entity) => <></>);
+const TransformComponent = createComponent("transform", { x: 0, y: 0, z: 0 });
+const TestComponent = createComponent("test", "");
+
+type Entity<T extends Component<string, any>[] = []> = {
+  [Property in `${number}` &
+    keyof T as T[Property]["name"]]: T[Property]["defaultValue"];
+};
+
+const EntityContext = createContext<Entity>(null);
+
+interface EntityProps {
+  render?: (typeof RenderComponent)["defaultValue"];
+}
+const Entity = forwardRef<Entity, EntityProps>((props, ref) => {
+  const entity = useRef<Entity<[]>>({
+    id: crypto.randomUUID(),
+    ...props,
+  });
+  useImperativeHandle(ref, () => entity.current, [props]);
+
+  useEffect(() => {}, [props]);
+
+  return (
+    <EntityContext.Provider value={entity.current}>
+      {props?.render(entity.current)}
+    </EntityContext.Provider>
+  );
+});
+
+let audioContext: AudioContext;
+const useAudio = (src?: string) => {
+  const isPlaying = useRef(false);
+
+  // check if audio context already exists
+  if (!audioContext) {
+    audioContext = new AudioContext();
+  }
+
+  let audioBuffer: AudioBuffer;
+  fetch(src)
+    .then((response) => {
+      return response.arrayBuffer();
+    })
+    .then((arrayBuffer) => {
+      return audioContext.decodeAudioData(arrayBuffer);
+    })
+    .then((_audioBuffer) => (audioBuffer = _audioBuffer));
+
+  const play = () => {
+    if (audioBuffer == null) {
+      return;
+    }
+
+    if (audioContext.state === "suspended") {
+      // we can't resume audio context in this function
+      console.warn("AudioContext is suspended.");
+      return;
+    }
+
+    if (isPlaying.current) {
+      // we are already playing
+      return;
+    }
+
+    // create new audio buffer source node and play!
+    isPlaying.current = true;
+    const audioBufferSourceNode = new AudioBufferSourceNode(audioContext, {
+      buffer: audioBuffer,
+    });
+    audioBufferSourceNode.onended = () => {
+      isPlaying.current = false;
+    };
+    audioBufferSourceNode.connect(audioContext.destination);
+    audioBufferSourceNode.start(audioContext.currentTime);
+  };
+
+  const onClick = () => {
+    if (audioContext?.state === "suspended") {
+      audioContext.resume();
+    }
+  };
+
+  // listen for 'click' event to resume a suspended AudioContext
+  useEffect(() => {
+    window.addEventListener("click", onClick);
+    return () => {
+      window.removeEventListener("click", onClick);
+    };
+  }, []);
+
+  return {
+    play,
+  };
+};
+
+//
+type InputEvent = {
+  keyboard: KeyboardEvent;
+  mouse: MouseEvent;
+  gamepads: Gamepad[];
+};
+const useInput = (
+  listener: (inputEvent: InputEvent) => void,
+  el?: HTMLElement
+) => {
+  const input = useRef<InputEvent>({
+    keyboard: null,
+    mouse: null,
+    gamepads: null,
+  });
+  const inputFrame = useRef(0);
+
+  const update = () => {
+    getGamepadInput();
+    listener(input.current);
+    inputFrame.current = requestAnimationFrame(update);
+  };
+
+  const onKeyDown = (event: Event) => {
+    input.current.keyboard = event as KeyboardEvent;
+  };
+  const onKeyUp = (event: Event) => {
+    input.current.keyboard = event as KeyboardEvent;
+  };
+  const onMouseMove = (event: Event) => {
+    input.current.mouse = event as MouseEvent;
+  };
+  const onMouseDown = (event: Event) => {
+    input.current.mouse = event as MouseEvent;
+  };
+  const onMouseUp = (event: Event) => {
+    input.current.mouse = event as MouseEvent;
+  };
+
+  const getGamepadInput = () => {
+    // 8bitDo controller buttons
+    // 0 1 2 3 4 5 6  7  8      9     10 11 12 13   14   15    16
+    // B A Y X L R L2 R2 Select Start L3 R3 Up Down Left Right Home
+    const gamepads = navigator.getGamepads();
+    input.current.gamepads = gamepads;
+  };
+
+  useEffect(() => {
+    const element = el ?? document;
+    element.addEventListener("keydown", onKeyDown);
+    element.addEventListener("keyup", onKeyUp);
+    element.addEventListener("mousemove", onMouseMove);
+    element.addEventListener("mousedown", onMouseDown);
+    element.addEventListener("mouseup", onMouseUp);
+
+    inputFrame.current = requestAnimationFrame(update);
+
+    () => {
+      element.removeEventListener("keydown", onKeyDown);
+      element.removeEventListener("keyup", onKeyUp);
+      element.removeEventListener("mousemove", onMouseMove);
+      element.removeEventListener("mousedown", onMouseDown);
+      element.removeEventListener("mouseup", onMouseUp);
+
+      cancelAnimationFrame(inputFrame.current);
+    };
+  }, []);
+};
+
+const FPS_60_MS = 1000 / 60;
+const useKey = (target: string, listener: () => void, continuous = false) => {
+  const keyInputRef = useRef(false);
+  const requestAnimationFrameRef = useRef(0);
+  const lastUpdateRef = useRef<number>(0);
+
+  const update = () => {
+    const now = Date.now();
+    const delta = now - lastUpdateRef.current;
+    if (keyInputRef.current && delta > FPS_60_MS) {
+      listener();
+      lastUpdateRef.current = now;
+    }
+    requestAnimationFrameRef.current = requestAnimationFrame(update);
+  };
+
+  const onKeydown = ({ key }: KeyboardEvent) => {
+    if (key === target && keyInputRef.current === false) {
+      keyInputRef.current = true;
+      if (!continuous) {
+        listener();
+      }
+    }
+  };
+
+  const onKeyup = ({ key }: KeyboardEvent) => {
+    if (key === target) {
+      keyInputRef.current = false;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", onKeydown);
+    window.addEventListener("keyup", onKeyup);
+    return () => {
+      window.removeEventListener("keydown", onKeydown);
+      window.removeEventListener("keyup", onKeyup);
+    };
+  }, [listener]);
+
+  useEffect(() => {
+    if (continuous === true) {
+      lastUpdateRef.current = Date.now();
+      requestAnimationFrameRef.current = requestAnimationFrame(update);
+    }
+    return () => {
+      cancelAnimationFrame(requestAnimationFrameRef.current);
+    };
+  }, [continuous, listener]);
+};
+
+// cf. https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/button#value
+// 0: Main button pressed, usually the left button or the un-initialized state
+// 1: Auxiliary button pressed, usually the wheel button or the middle button (if present)
+// 2: Secondary button pressed, usually the right button
+// 3: Fourth button, typically the Browser Back button
+// 4: Fifth button, typically the Browser Forward button
+const MouseButtonMap = {
+  0: "left",
+  1: "wheel",
+  2: "right",
+  3: "back",
+  4: "forward",
+} as const;
+type MouseButtonMapKey = keyof typeof MouseButtonMap;
+type MouseButton = (typeof MouseButtonMap)[MouseButtonMapKey];
+
+type MiniMouseEvent = {
+  button: MouseButton | undefined | null;
+  x: number;
+  y: number;
+};
+
+const useMouse = (
+  target: MouseButton | "move",
+  listener: (ev: MiniMouseEvent) => void,
+  continuous = false
+) => {
+  const mouseEventRef = useRef<MiniMouseEvent>({
+    button: null,
+    x: null,
+    y: null,
+  });
+  const requestAnimationFrameRef = useRef(0);
+  const lastUpdateRef = useRef<number>(0);
+
+  const update = () => {
+    const now = Date.now();
+    const delta = now - lastUpdateRef.current;
+    if (mouseEventRef.current?.button === target && delta > FPS_60_MS) {
+      listener(mouseEventRef.current);
+      lastUpdateRef.current = now;
+    }
+    requestAnimationFrameRef.current = requestAnimationFrame(update);
+  };
+
+  const onMousedown = ({ button, clientX, clientY }: MouseEvent) => {
+    mouseEventRef.current = {
+      button: MouseButtonMap[button as MouseButtonMapKey],
+      x: clientX,
+      y: clientY,
+    };
+    if (mouseEventRef.current?.button === target) {
+      listener(mouseEventRef.current);
+    }
+  };
+
+  const onMouseup = ({ button, clientX, clientY }: MouseEvent) => {
+    mouseEventRef.current = {
+      button: null,
+      x: clientX,
+      y: clientY,
+    };
+  };
+
+  const onMousemove = ({ button, clientX, clientY }: MouseEvent) => {
+    mouseEventRef.current = {
+      ...mouseEventRef.current,
+      x: clientX,
+      y: clientY,
+    };
+    if (target === "move") {
+      listener(mouseEventRef.current);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousedown", onMousedown);
+    window.addEventListener("mouseup", onMouseup);
+    window.addEventListener("mousemove", onMousemove);
+    return () => {
+      window.removeEventListener("mousedown", onMousedown);
+      window.removeEventListener("mouseup", onMouseup);
+      window.addEventListener("mousemove", onMousemove);
+    };
+  }, [listener]);
+
+  useEffect(() => {
+    if (continuous === true) {
+      lastUpdateRef.current = Date.now();
+      requestAnimationFrameRef.current = requestAnimationFrame(update);
+    }
+    return () => {
+      cancelAnimationFrame(requestAnimationFrameRef.current);
+    };
+  }, [continuous, listener]);
+};
+
+// 8bitDo controller buttons
+// 0 1 2 3 4 5 6  7  8      9     10 11 12 13   14   15    16
+// B A Y X L R L2 R2 Select Start L3 R3 Up Down Left Right Home
+const GamepadButtonMap = {
+  0: "B",
+  1: "A",
+  2: "Y",
+  3: "X",
+  4: "L",
+  5: "R",
+  6: "L2",
+  7: "R2",
+  8: "Select",
+  9: "Start",
+  10: "L3",
+  11: "R3",
+  12: "Up",
+  13: "Down",
+  14: "Left",
+  15: "Right",
+  16: "Home", // unnecessary?
+} as const;
+type GamepadButtonMapKey = keyof typeof GamepadButtonMap;
+type GamepadButton = (typeof GamepadButtonMap)[GamepadButtonMapKey];
+
+const useGamepad = (
+  target: GamepadButton,
+  listener: () => void,
+  continuous = false
+) => {
+  const buttonIndex = Object.keys(GamepadButtonMap).find(
+    (key) => GamepadButtonMap[key as `${GamepadButtonMapKey}`] === target
+  );
+  const gamepadPressedRef = useRef(false);
+  const requestAnimationFrameRef = useRef(0);
+  const lastUpdateRef = useRef<number>(0);
+
+  const update = () => {
+    const gamepads = navigator.getGamepads();
+    const lastPressed = gamepadPressedRef.current;
+    gamepadPressedRef.current = gamepads?.[0]?.buttons?.[+buttonIndex]?.pressed;
+
+    const now = Date.now();
+    const delta = now - lastUpdateRef.current;
+    if (
+      delta > FPS_60_MS &&
+      ((continuous && gamepadPressedRef.current === true) ||
+        (lastPressed === false && gamepadPressedRef.current === true))
+    ) {
+      console.log("update");
+      listener();
+      lastUpdateRef.current = now;
+    }
+    requestAnimationFrameRef.current = requestAnimationFrame(update);
+  };
+
+  useEffect(() => {
+    lastUpdateRef.current = Date.now();
+    requestAnimationFrameRef.current = requestAnimationFrame(update);
+    return () => {
+      cancelAnimationFrame(requestAnimationFrameRef.current);
+    };
+  }, [listener]);
+};
--- packages/react-gamin/src/core/entities.tsx
+++ packages/react-gamin/src/core/entities.tsx
@@ -11,83 +11,81 @@ import {
 } from "react";
 import { useGame } from "./game";
 
-interface EntitiesProps {
-  entities: React.ReactNode[];
-}
-export function Entities({ entities }: EntitiesProps) {
-  return (
-    <>
-      {entities.map((entity, i) => (
-        <Entity key={i}>{entity}</Entity>
-      ))}
-    </>
-  );
+// From https://stackoverflow.com/a/50375286
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (
+  k: infer I
+) => void
+  ? I
+  : never;
+
+// From: https://stackoverflow.com/a/53955431
+type IsUnion<T> = [T] extends [UnionToIntersection<T>] ? false : true;
+
+// Here we come!
+type SingleKey<T> = IsUnion<keyof T> extends true
+  ? never
+  : {} extends T
+  ? never
+  : T;
+
+type Entity<T extends Record<PropertyKey, any>[]> = {
+  id: string;
+} & UnionToIntersection<T[number]>;
+
+type Component<T> = SingleKey<T>;
+
+export const Transform = {
+  transform: {
+    x: 0,
+    y: 0,
+    z: 0,
+  },
+};
+
+export const Body = {
+  body: {
+    width: 100,
+    height: 100,
+  },
+};
+
+export function useComponent<T>(
+  component: Component<T>,
+  initialValue?: T[keyof T]
+) {
+  const property = Object.keys(component)[0];
+  let entity = useEntity();
+
+  // passing useState() a function that returns the initial value will
+  // ensure components can store functions as state as well, instead of
+  // useState() interpretting the function as a state setter
+  const state = useState(() => initialValue ?? Object.values(component)[0]);
+
+  useEffect(() => {
+    entity = Object.assign(
+      {},
+      entity,
+      { [property]: initialValue } ?? component
+    );
+  }, []);
+
+  return state as [T[keyof T], Dispatch<React.SetStateAction<T[keyof T]>>];
 }
 
-// this type is magic!
-export type EntityComponentsMap<T extends Component<any, any>[]> =
-  T extends undefined
-    ? Record<string, any>
-    : {
-        [Key in `${number}` &
-          keyof T as T[Key]["name"]]: T[Key]["type"] extends Function
-          ? // Readonly<T[Key]["type"]> somehow removes call signatures,
-            // so functions being stored here will not be callable despite
-            // not being modified. cf. https://github.com/microsoft/TypeScript/issues/32566
-            Function
-          : Readonly<T[Key]["type"]>;
-      };
-export const EntityContext = createContext<IEntity>(null);
-export interface IEntity<T extends Component<any, any>[] = any> {
-  _id: string;
-  _state: Record<PropertyKey, [any, Dispatch<any>]>;
-  components: EntityComponentsMap<T>;
-  update: <K extends string, T>(
-    componentType: Component<K, T>,
-    data: T
-  ) => void;
-  has: (...components: T[]) => boolean;
-}
+const getE = <T extends SingleKey<T>[]>(...types: T): Entity<T> => {
+  return { id: 3 } as Entity<T>;
+};
+
+export const EntityContext = createContext<Entity<any>>(null);
 export interface EntityProps extends PropsWithChildren {}
-export const Entity = forwardRef<IEntity, EntityProps>(function Entity(
+export const Entity = forwardRef<Entity<any>, EntityProps>(function Entity(
   { children },
   ref
 ) {
-  const entityRef = useRef<IEntity>({
-    _id: crypto.randomUUID(),
-    _state: {},
-    components: {},
-    update: null,
-    has: null,
+  const entityRef = useRef<Entity<any>>({
+    id: crypto.randomUUID(),
   });
 
-  const has = <T extends Component<any, any>[]>(...components: T) => {
-    for (const component of components) {
-      if (entityRef.current.components[component.name] == null) {
-        return false;
-      }
-    }
-
-    return true;
-  };
-  entityRef.current.has = has;
-
-  const update = <K extends string, T>(
-    componentType: Component<K, T>,
-    data: T
-  ) => {
-    const component = entityRef.current._state[componentType.name];
-    if (!component) {
-      console.warn(
-        `Entity ${entityRef.current._id} does not have component ${componentType.name}`
-      );
-      return;
-    }
-
-    const [, setState] = component;
-    setState(data);
-  };
-  entityRef.current.update = update;
   useImperativeHandle(ref, () => entityRef.current);
 
   const game = useGame();
@@ -105,90 +103,41 @@ export const Entity = forwardRef<IEntity, EntityProps>(function Entity(
   );
 });
 
-export function useEntity<T extends Component<any, any>[] = []>() {
+export function useEntity<T = {}>() {
   const context = useContext(EntityContext);
   if (!context) {
     console.warn("useEntity must be used inside a <Entity /> context.");
   }
-  return context as IEntity<T>;
-}
-
-export type Component<K extends string, T> = {
-  readonly name: K;
-  readonly type: T;
-};
-
-export function createComponent<K extends string, T>(
-  name: K,
-  type: T
-): Component<K, T> {
-  return {
-    name,
-    type,
-  };
-}
-
-export function useComponent<K extends string, T>(name: K, intialValue?: T) {
-  const entity = useEntity<[any]>();
-  //                         ^
-  // TODO: This is a hack I'm already seeing too often
-
-  // passing useState() a function that returns the initial value will
-  // ensure components can store functions as state as well, instead of
-  // useState() interpretting the function as a state setter
-  const [state, setState] = useState(() => intialValue);
-
-  useEffect(() => {
-    entity.components[name] = state;
-    entity._state[name] = [state, setState];
-  }, [state]);
-
-  return [state, setState] as [T, Dispatch<React.SetStateAction<T>>];
+  return context as Entity<[T]>;
 }
 
 // default components
-export interface Transform {
-  x: number;
-  y: number;
-  z: number;
-}
+// export interface Velocity {
+//   dx: number;
+//   dy: number;
+//   dz: number;
+// }
 
-export const TransformComponent = createComponent("transform", {
-  x: 0,
-  y: 0,
-  z: 0,
-});
+// export const VelocityComponent = createComponent("velocity", {
+//   dx: 0,
+//   dy: 0,
+//   dz: 0,
+// });
 
-export function useTransformComponent(initialValue?: Transform) {
-  return useComponent("transform", initialValue);
-}
+// export function useVelocityComponent(initialValue?: Velocity) {
+//   return useComponent("velocity", initialValue);
+// }
 
-export interface Velocity {
-  dx: number;
-  dy: number;
-  dz: number;
-}
+// export interface Body {
+//   height: number;
+//   width: number;
+// }
 
-export const VelocityComponent = createComponent("velocity", {
-  dx: 0,
-  dy: 0,
-  dz: 0,
-});
+// export const BodyComponent = createComponent("body", {
+//   width: 100,
+//   height: 100,
+// });
 
-export function useVelocityComponent(initialValue?: Velocity) {
-  return useComponent("velocity", initialValue);
-}
-
-export interface Body {
-  height: number;
-  width: number;
-}
-
-export const BodyComponent = createComponent("body", {
-  width: 100,
-  height: 100,
-});
-
-export function useBodyComponent(initialValue?: Body) {
-  return useComponent("body", initialValue);
-}
+// export function useBodyComponent(initialValue?: Body) {
+//   return useComponent("body", initialValue);
+// }
