59553a93eb579546d861a15eda6f4f74ff3df3a1
Jonathon Orsi <jonathon.orsi@gmail.com>
2023-05-01 23:35:11 -0400

    update useKey type; add stop to useAudio

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,18 +7,21 @@ import {
   useContext,
   useState,
   useCallback,
-} from "react";
+} from 'react';
+import { KeyboardCode } from './keys';
 
 export default function App() {
   const ref = useRef<Entity>();
   const [transform, setTransform] = useState({ x: 500, y: 200, z: 0 });
 
   // audio
-  const sample = useAudio("/beep-03.wav");
+  const sample = useAudio('/beep-03.wav');
+  const satie = useAudio('/Gymnopedie_No._1.mp3');
 
-  useGamepad(
-    "Up",
+  useKey(
+    'ArrowUp',
     () => {
+      satie.play();
       setTransform({
         ...transform,
         y: transform.y - 5,
@@ -27,9 +30,10 @@ export default function App() {
     true
   );
 
-  useGamepad(
-    "Down",
+  useKey(
+    'ArrowDown',
     () => {
+      satie.stop();
       setTransform({
         ...transform,
         y: transform.y + 5,
@@ -38,9 +42,10 @@ export default function App() {
     true
   );
 
-  useGamepad(
-    "Left",
+  useKey(
+    'ArrowLeft',
     () => {
+      sample.play();
       setTransform({
         ...transform,
         x: transform.x - 5,
@@ -49,8 +54,8 @@ export default function App() {
     true
   );
 
-  useGamepad(
-    "Right",
+  useKey(
+    'ArrowRight',
     () => {
       setTransform({
         ...transform,
@@ -64,12 +69,12 @@ export default function App() {
   return (
     <div
       style={{
-        position: "absolute",
+        position: 'absolute',
         top: `${transform.y}px`,
         left: `${transform.x}px`,
-        background: "white",
-        width: "50px",
-        height: "50px",
+        background: 'white',
+        width: '50px',
+        height: '50px',
       }}
     ></div>
   );
@@ -98,23 +103,23 @@ const useComponent = <K extends string, T>(component: Component<K, T>): T => {
     return;
   }
 
-  return entity[component["name"]];
+  return entity[component['name']];
 };
 
-const IdComponent = createComponent("id", "");
-const RenderComponent = createComponent("render", (entity: Entity) => <></>);
-const TransformComponent = createComponent("transform", { x: 0, y: 0, z: 0 });
-const TestComponent = createComponent("test", "");
+const IdComponent = createComponent('id', '');
+const RenderComponent = createComponent('render', (entity: Entity) => <></>);
+const TransformComponent = createComponent('transform', { x: 0, y: 0, z: 0 });
+const TestComponent = createComponent('test', '');
 
 type Entity<T extends Component<string, any>[] = []> = {
   [Property in `${number}` &
-    keyof T as T[Property]["name"]]: T[Property]["defaultValue"];
+    keyof T as T[Property]['name']]: T[Property]['defaultValue'];
 };
 
 const EntityContext = createContext<Entity>(null);
 
 interface EntityProps {
-  render?: (typeof RenderComponent)["defaultValue"];
+  render?: typeof RenderComponent['defaultValue'];
 }
 const Entity = forwardRef<Entity, EntityProps>((props, ref) => {
   const entity = useRef<Entity<[]>>({
@@ -134,6 +139,8 @@ const Entity = forwardRef<Entity, EntityProps>((props, ref) => {
 
 let audioContext: AudioContext;
 const useAudio = (src?: string) => {
+  const audioBufferRef = useRef<AudioBuffer>();
+  const audioBufferSourceNodeRef = useRef<AudioBufferSourceNode>();
   const isPlaying = useRef(false);
 
   // check if audio context already exists
@@ -141,60 +148,67 @@ const useAudio = (src?: string) => {
     audioContext = new AudioContext();
   }
 
-  let audioBuffer: AudioBuffer;
-  fetch(src)
-    .then((response) => {
-      return response.arrayBuffer();
-    })
-    .then((arrayBuffer) => {
-      return audioContext.decodeAudioData(arrayBuffer);
-    })
-    .then((_audioBuffer) => (audioBuffer = _audioBuffer));
+  if (!audioBufferRef.current) {
+    fetch(src)
+      .then((response) => {
+        return response.arrayBuffer();
+      })
+      .then((arrayBuffer) => {
+        return audioContext.decodeAudioData(arrayBuffer);
+      })
+      .then((audioBuffer) => (audioBufferRef.current = audioBuffer));
+  }
+
+  //  setup window 'click' event to resume a suspended AudioContext
+  const onClick = () => {
+    if (audioContext?.state === 'suspended') {
+      audioContext.resume();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', onClick);
+    return () => {
+      window.removeEventListener('click', onClick);
+    };
+  }, []);
 
   const play = () => {
-    if (audioBuffer == null) {
+    if (audioBufferRef.current == null) {
       return;
     }
 
-    if (audioContext.state === "suspended") {
+    if (audioContext.state === 'suspended') {
       // we can't resume audio context in this function
-      console.warn("AudioContext is suspended.");
+      console.warn('AudioContext is suspended.');
       return;
     }
 
-    if (isPlaying.current) {
+    if (audioBufferSourceNodeRef.current != null && isPlaying.current) {
       // we are already playing
       return;
     }
 
     // create new audio buffer source node and play!
     isPlaying.current = true;
-    const audioBufferSourceNode = new AudioBufferSourceNode(audioContext, {
-      buffer: audioBuffer,
+    audioBufferSourceNodeRef.current = new AudioBufferSourceNode(audioContext, {
+      buffer: audioBufferRef.current,
     });
-    audioBufferSourceNode.onended = () => {
+    audioBufferSourceNodeRef.current.onended = () => {
       isPlaying.current = false;
     };
-    audioBufferSourceNode.connect(audioContext.destination);
-    audioBufferSourceNode.start(audioContext.currentTime);
+    audioBufferSourceNodeRef.current.connect(audioContext.destination);
+    audioBufferSourceNodeRef.current.start(audioContext.currentTime);
   };
 
-  const onClick = () => {
-    if (audioContext?.state === "suspended") {
-      audioContext.resume();
-    }
+  const stop = () => {
+    audioBufferSourceNodeRef.current?.stop();
+    isPlaying.current = false;
   };
-
-  // listen for 'click' event to resume a suspended AudioContext
-  useEffect(() => {
-    window.addEventListener("click", onClick);
-    return () => {
-      window.removeEventListener("click", onClick);
-    };
-  }, []);
 
   return {
     play,
+    stop,
   };
 };
 
@@ -247,20 +261,20 @@ const useInput = (
 
   useEffect(() => {
     const element = el ?? document;
-    element.addEventListener("keydown", onKeyDown);
-    element.addEventListener("keyup", onKeyUp);
-    element.addEventListener("mousemove", onMouseMove);
-    element.addEventListener("mousedown", onMouseDown);
-    element.addEventListener("mouseup", onMouseUp);
+    element.addEventListener('keydown', onKeyDown);
+    element.addEventListener('keyup', onKeyUp);
+    element.addEventListener('mousemove', onMouseMove);
+    element.addEventListener('mousedown', onMouseDown);
+    element.addEventListener('mouseup', onMouseUp);
 
     inputFrame.current = requestAnimationFrame(update);
 
     () => {
-      element.removeEventListener("keydown", onKeyDown);
-      element.removeEventListener("keyup", onKeyUp);
-      element.removeEventListener("mousemove", onMouseMove);
-      element.removeEventListener("mousedown", onMouseDown);
-      element.removeEventListener("mouseup", onMouseUp);
+      element.removeEventListener('keydown', onKeyDown);
+      element.removeEventListener('keyup', onKeyUp);
+      element.removeEventListener('mousemove', onMouseMove);
+      element.removeEventListener('mousedown', onMouseDown);
+      element.removeEventListener('mouseup', onMouseUp);
 
       cancelAnimationFrame(inputFrame.current);
     };
@@ -268,7 +282,11 @@ const useInput = (
 };
 
 const FPS_60_MS = 1000 / 60;
-const useKey = (target: string, listener: () => void, continuous = false) => {
+const useKey = (
+  target: KeyboardCode,
+  listener: () => void,
+  continuous = false
+) => {
   const keyInputRef = useRef(false);
   const requestAnimationFrameRef = useRef(0);
   const lastUpdateRef = useRef<number>(0);
@@ -283,7 +301,7 @@ const useKey = (target: string, listener: () => void, continuous = false) => {
     requestAnimationFrameRef.current = requestAnimationFrame(update);
   };
 
-  const onKeydown = ({ key }: KeyboardEvent) => {
+  const onKeydown = ({ code, key }: KeyboardEvent) => {
     if (key === target && keyInputRef.current === false) {
       keyInputRef.current = true;
       if (!continuous) {
@@ -292,18 +310,18 @@ const useKey = (target: string, listener: () => void, continuous = false) => {
     }
   };
 
-  const onKeyup = ({ key }: KeyboardEvent) => {
+  const onKeyup = ({ code, key }: KeyboardEvent) => {
     if (key === target) {
       keyInputRef.current = false;
     }
   };
 
   useEffect(() => {
-    window.addEventListener("keydown", onKeydown);
-    window.addEventListener("keyup", onKeyup);
+    window.addEventListener('keydown', onKeydown);
+    window.addEventListener('keyup', onKeyup);
     return () => {
-      window.removeEventListener("keydown", onKeydown);
-      window.removeEventListener("keyup", onKeyup);
+      window.removeEventListener('keydown', onKeydown);
+      window.removeEventListener('keyup', onKeyup);
     };
   }, [listener]);
 
@@ -325,14 +343,14 @@ const useKey = (target: string, listener: () => void, continuous = false) => {
 // 3: Fourth button, typically the Browser Back button
 // 4: Fifth button, typically the Browser Forward button
 const MouseButtonMap = {
-  0: "left",
-  1: "wheel",
-  2: "right",
-  3: "back",
-  4: "forward",
+  0: 'left',
+  1: 'wheel',
+  2: 'right',
+  3: 'back',
+  4: 'forward',
 } as const;
 type MouseButtonMapKey = keyof typeof MouseButtonMap;
-type MouseButton = (typeof MouseButtonMap)[MouseButtonMapKey];
+type MouseButton = typeof MouseButtonMap[MouseButtonMapKey];
 
 type MiniMouseEvent = {
   button: MouseButton | undefined | null;
@@ -341,7 +359,7 @@ type MiniMouseEvent = {
 };
 
 const useMouse = (
-  target: MouseButton | "move",
+  target: MouseButton | 'move',
   listener: (ev: MiniMouseEvent) => void,
   continuous = false
 ) => {
@@ -388,19 +406,19 @@ const useMouse = (
       x: clientX,
       y: clientY,
     };
-    if (target === "move") {
+    if (target === 'move') {
       listener(mouseEventRef.current);
     }
   };
 
   useEffect(() => {
-    window.addEventListener("mousedown", onMousedown);
-    window.addEventListener("mouseup", onMouseup);
-    window.addEventListener("mousemove", onMousemove);
+    window.addEventListener('mousedown', onMousedown);
+    window.addEventListener('mouseup', onMouseup);
+    window.addEventListener('mousemove', onMousemove);
     return () => {
-      window.removeEventListener("mousedown", onMousedown);
-      window.removeEventListener("mouseup", onMouseup);
-      window.addEventListener("mousemove", onMousemove);
+      window.removeEventListener('mousedown', onMousedown);
+      window.removeEventListener('mouseup', onMouseup);
+      window.addEventListener('mousemove', onMousemove);
     };
   }, [listener]);
 
@@ -419,26 +437,26 @@ const useMouse = (
 // 0 1 2 3 4 5 6  7  8      9     10 11 12 13   14   15    16
 // B A Y X L R L2 R2 Select Start L3 R3 Up Down Left Right Home
 const GamepadButtonMap = {
-  0: "B",
-  1: "A",
-  2: "Y",
-  3: "X",
-  4: "L",
-  5: "R",
-  6: "L2",
-  7: "R2",
-  8: "Select",
-  9: "Start",
-  10: "L3",
-  11: "R3",
-  12: "Up",
-  13: "Down",
-  14: "Left",
-  15: "Right",
-  16: "Home", // unnecessary?
+  0: 'B',
+  1: 'A',
+  2: 'Y',
+  3: 'X',
+  4: 'L',
+  5: 'R',
+  6: 'L2',
+  7: 'R2',
+  8: 'Select',
+  9: 'Start',
+  10: 'L3',
+  11: 'R3',
+  12: 'Up',
+  13: 'Down',
+  14: 'Left',
+  15: 'Right',
+  16: 'Home', // unnecessary?
 } as const;
 type GamepadButtonMapKey = keyof typeof GamepadButtonMap;
-type GamepadButton = (typeof GamepadButtonMap)[GamepadButtonMapKey];
+type GamepadButton = typeof GamepadButtonMap[GamepadButtonMapKey];
 
 const useGamepad = (
   target: GamepadButton,
@@ -464,7 +482,7 @@ const useGamepad = (
       ((continuous && gamepadPressedRef.current === true) ||
         (lastPressed === false && gamepadPressedRef.current === true))
     ) {
-      console.log("update");
+      console.log('update');
       listener();
       lastUpdateRef.current = now;
     }
